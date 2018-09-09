ea53af4be661da8eeb670ab42e37a0f8397b6d4b
Kevin Ball <kball8000@gmail.com>
2018-09-09 15:47:39 -0500

    Updated so sw now includes all icons instead of using the cache algorithm.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let CACHE_NAME = 'sonny-v1.30l';
+let CACHE_NAME = 'sonny-v1.30m';
 
 /**
  * Caching essential assets seems critical, otherwise requires user to have browsed
@@ -13,14 +13,60 @@ let urlsToCache = [
     '/radar',
     '/month',
     '/css/sonny.css',
+    '/css/lib/angular-material.min.css',
+    '/css/lib/bootstrap.min.css',
     '/favicon.ico',
     '/js/controllers.js',
+    '/js/main.js',
     '/js/services.js',
+    '/js/lib/angular.min.js',
+    '/js/lib/angular-animate.min.js',
+    '/js/lib/angular-aria.min.js',
+    '/js/lib/angular-material.min.js',
+    '/js/lib/angular-route.min.js',
     '/partials/current.html',
     '/partials/hourly.html',
     '/partials/tenday.html',
     '/partials/month.html',
-    '/partials/radar.html'
+    '/partials/radar.html',
+    'images/icons/fog.gif',
+    'images/icons/hazy.gif',
+    'images/icons/clear.gif',
+    'images/icons/sunny.gif',
+    'images/icons/mostlysunny.gif',
+    'images/icons/partlycloudy.gif',
+    'images/icons/partlysunny.gif',
+    'images/icons/mostlycloudy.gif',
+    'images/icons/cloudy.gif',
+    'images/icons/chancerain.gif',
+    'images/icons/rain.gif',
+    'images/icons/chancetstorms.gif',
+    'images/icons/tstorms.gif',
+    'images/icons/chanceflurries.gif',
+    'images/icons/flurries.gif',
+    'images/icons/chancesleet.gif',
+    'images/icons/sleet.gif',
+    'images/icons/chancesnow.gif',
+    'images/icons/snow.gif',
+    'images/icons/nt_fog.gif',
+    'images/icons/nt_hazy.gif',
+    'images/icons/nt_clear.gif',
+    'images/icons/nt_sunny.gif',
+    'images/icons/nt_mostlysunny.gif',
+    'images/icons/nt_partlycloudy.gif',
+    'images/icons/nt_partlysunny.gif',
+    'images/icons/nt_mostlycloudy.gif',
+    'images/icons/nt_cloudy.gif',
+    'images/icons/nt_chancerain.gif',
+    'images/icons/nt_rain.gif',
+    'images/icons/nt_chancetstorms.gif',
+    'images/icons/nt_tstorms.gif',
+    'images/icons/nt_chanceflurries.gif',
+    'images/icons/nt_flurries.gif',
+    'images/icons/nt_chancesleet.gif',
+    'images/icons/nt_sleet.gif',
+    'images/icons/nt_chancesnow.gif',
+    'images/icons/nt_snow.gif'
   ];
 
 self.addEventListener('install', function(event){
@@ -52,17 +98,17 @@ self.addEventListener('activate', function(event){
 
 // Returns file from cache for a fetch/GET request, not POST, as POST is not supported
 // Helpful document: https://developers.google.com/web/fundamentals/primers/service-workers/
-self.addEventListener('fetch', function(event) {  
-  event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        return caches.open(CACHE_NAME).then(function(cache) {
-          if (event.request.method === 'GET') {    // only difference from help doc above.
-            cache.put(event.request, response.clone());
-          }            
-          return response;
-        });  
-      });
-    })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(resp) {
+//       return resp || fetch(event.request).then(function(response) {
+//         return caches.open(CACHE_NAME).then(function(cache) {
+//           if (event.request.method === 'GET') {    // only difference from help doc above.
+//             cache.put(event.request, response.clone());
+//           }            
+//           return response;
+//         });  
+//       });
+//     })
+//   );
+// });
